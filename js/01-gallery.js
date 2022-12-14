0d2479b2580caf3656c0dea51d1aa10297f8d7f1
Anatolij Goga <anzefal@gmail.com>
2022-12-14 20:24:43 +0200

    Added html markup with JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListElement = document.querySelector('.gallery');
+const galleryMarkup = creatGalleryItemsMarkup(galleryItems);
+
+galleryListElement.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function creatGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, description }) => {
+      return `<li><img src = '${preview}' alt = '${description}' width=320></li>`;
+    })
+    .join('');
+}
