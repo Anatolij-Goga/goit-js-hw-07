ef19f81d7135b1a96894987f28d52b1b9073ec76
Anatolij Goga <anzefal@gmail.com>
2022-12-15 20:34:25 +0200

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,8 @@ const galleryMarkup = creatGalleryItemsMarkup(galleryItems);
 
 galleryListElement.insertAdjacentHTML('beforeend', galleryMarkup);
 
+galleryListElement.addEventListener('click', onGalleryContainerClick);
+
 function creatGalleryItemsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -22,4 +24,23 @@ function creatGalleryItemsMarkup(galleryItems) {
 </div>`;
     })
     .join('');
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery')) return;
+
+  const source = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+
+  galleryListElement.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
 }
