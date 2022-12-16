747859d26a3f09ff36b37c159db9684f41625e27
Anatolij Goga <anzefal@gmail.com>
2022-12-16 20:13:28 +0200

    Task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -44,3 +44,5 @@ function onGalleryContainerClick(event) {
     }
   });
 }
+
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryListElement = document.querySelector('.gallery');
+const galleryMarkup = creatGalleryItemsMarkup(galleryItems);
+galleryListElement.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function creatGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>
+     
+    </ul>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
+
 console.log(galleryItems);
